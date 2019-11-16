2a12873c63a57df9566d551cc92da58e4b5e0585
Kevin Downs <kevin.downs04@gmail.com>
2019-11-16 23:32:06 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -19,7 +19,7 @@ class Patient
   end
   
   def doctors
-    self.appointments.collect{|appointment| appointment.doctor}
+    self.appointments.collect{|appointment| appointment.doct}
   end
   
   def self.all 
