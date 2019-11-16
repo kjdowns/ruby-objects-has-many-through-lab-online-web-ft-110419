80062d1e020aa15984d70e1aa92f07c552c21623
Kevin Downs <kevin.downs04@gmail.com>
2019-11-16 23:28:32 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -14,6 +14,10 @@ class Patient
     Appointment.new(date, self, doctor)
   end
   
+  def appointments
+    Appointment.all.select{|appointment| appointment.patient == self}
+  end
+  
   def self.all 
     @@all
   end
