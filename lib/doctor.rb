5989fa081b5c1ffa3fe03a02b7eddc4cbe0f6de7
Kevin Downs <kevin.downs04@gmail.com>
2019-11-16 23:25:57 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -14,7 +14,7 @@ class Doctor
     Appointment.all.select {|appointment| appointment.doctor == self}
   end
   
-  def new_appointment(da, patient)
+  def new_appointment(patient, date)
     Appointment.new(date, patient, self)
   end
   
